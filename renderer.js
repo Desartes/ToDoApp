55b8183f53b07f8ce9c4d5131dd47cd3a5167077
Filip Kolesár <koleseri.koleseri@gmail.com>
2016-05-16 16:05:39 +0200

    Added "important notes" feature (+known issues)

--- renderer.js
+++ renderer.js
@@ -5,52 +5,63 @@ var remote = require('electron').remote;
 
 $('span').click(function(){
 	if ( $(this).attr('id') == 'close' ) {
-		remote.getCurrentWindow().close()
+		remote.getCurrentWindow().close();
 	} 
 	if ( $(this).attr('id') == 'minimize' ) {
-		remote.getCurrentWindow().minimize()
+		remote.getCurrentWindow().minimize();
 	}
-})
+});
 
 $('input[type=text]').on('keydown', function(e) {
+	var str;
+	if ( e.which == 13 && (str = $(this).val()) ) {
+		var date = GetTodayDate();
+		
+		if ( str.match("^:!") ) {
+			str = (str.split(":!"))[1];
+			var newitem = '<span class="date">' + date + '</span><div class="box important">' + str + '</div>';
+		} else {
+			var newitem = '<span class="date">' + date + '</span><div class="box">' + str + '</div>';
+		}
+
 	if ( e.which == 13 && $(this).val() ) {
 		var date = GetTodayDate()
 		var newval = $('<div />').html($(this).val()).html()
 		var newitem = '<span class="date">' + date + '</span><div class="box">' + newval + '</div>'
 
-		localStorage.setItem(date, newitem)
+		localStorage.setItem(date, newitem);
 
-		$('div.box').last().after(newitem)
-		$(this).val("")
-		e.preventDefault()
+		$('div.box').last().after(newitem);
+		$(this).val("");
+		e.preventDefault();
 	}
-})
+});
 
 function GetTodayDate() {
-   var tdate = new Date()
-   var dd = tdate.getDate() //yields day
-   var MM = tdate.getMonth() //yields month
-   var yyyy = tdate.getFullYear() //yields year
-   var hrs = tdate.getHours() //yields year
-   var mins = tdate.getMinutes() //yields year
-   var xxx = dd + "-" +( MM+1) + "-" + yyyy + " " + hrs + ":" + mins
+   var tdate = new Date();
+   var dd = tdate.getDate(); //yields day
+   var MM = tdate.getMonth(); //yields month
+   var yyyy = tdate.getFullYear(); //yields year
+   var hrs = tdate.getHours(); //yields year
+   var mins = tdate.getMinutes(); //yields year
+   var xxx = dd + "-" +( MM+1) + "-" + yyyy + " " + hrs + ":" + mins;
 
    return xxx;
 }
 
 $(window).scroll(function(event) {
-    if ( $(document).scrollTop() > 0 ) {
-    	$('#panel').css({ borderBottom:'2px solid #304057' })
+    if ( $(document).scrollTop() > 0 ) {
+    	$('#panel').css({ borderBottom:'2px solid #304057' });
     } else {
-    	$('#panel').css({ borderBottom:0 })
+    	$('#panel').css({ borderBottom:0 });
     }
 })
 
 function getNotes() {
 	if ( localStorage.length ) {
 		$.each(localStorage, function(key, value){
-			$('body').append(localStorage.getItem(key))
-		})
+			$('body').append(localStorage.getItem(key));
+		});
 	}
 }
 
